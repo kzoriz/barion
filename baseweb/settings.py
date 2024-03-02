960ea5e38e72a123ab15df6d50c51638b3ef0c81
kzoriz <kzoriz@outlook.com>
2024-03-02 12:07:13 -0300

    make it better

--- baseweb/settings.py
+++ baseweb/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 DEBUG = bool(os.environ.get('DEBUG', '').lower() == 'true')
 # DEBUG = S3Connection(os.environ['DEBUG'], )
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '')
 # ALLOWED_HOSTS = []
 
 # Application definition
