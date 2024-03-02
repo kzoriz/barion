eb67ad53c4744e4108d118fe2e0b5d6d5cb53554
kzoriz <kzoriz@outlook.com>
2024-03-02 09:39:41 -0300

    make it better

--- baseweb/settings.py
+++ baseweb/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!z-q&__fc3@r4awfv4)c$4-=!o824^^n9wt$&uxn*l*biv=)7n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
